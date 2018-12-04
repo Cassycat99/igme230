7e6facc37e9c3ed2629b9a80e3fd2d360e81cd68
Cassycat99 <Cassycat99@yahoo.com>
2018-12-04 18:37:28 -0500

    changing content

--- fall2018sample/fall2018sample.js
+++ fall2018sample/fall2018sample.js
@@ -11,14 +11,19 @@
 
 let content = ("content1.txt")  // sets default content1 element
 
-$("content").load(content);   // retrieves only default element
+$("content").load(content);   // loads content element with content.text1
 
 $("#content-menu").change(function() {
     content = $(this).val();
-$("#choose-content").load(content); 
+$("#content").load(content); 
 });
 
+let verse = ("verse 3-4.txt")  // sets default verse element
+$("#verse-choose").val(verse); // changes menu option to default
+$("#selectedverse").load(verse);   // retrieves only default element
 
-
+$("#verse-choose").change(function() {
+verse = $(this).val();
+$("#selectedverse").load(verse); 
 
 
