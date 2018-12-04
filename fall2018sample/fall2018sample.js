4d29e232416dd80c39f8854e8426d22e6cc84674
Cassy Smithies (RIT Student) <cls8313@ad.rit.edu>
2018-12-04 15:13:42 -0500

    adding JS

--- fall2018sample/fall2018sample.js
+++ fall2018sample/fall2018sample.js
@@ -1,7 +1,19 @@
 /* Don't use <script> tags in a linked js file! */
 
+/*          
+<label for="content-menu">Select a file to load:</label>
+<select name="content-menu" id="choose-content">
+<option value="content1.txt">Your Name</option>
+<option value="content2.txt">Course Info</option>
+</select>
+*/
 
 
-let name = "content1.txt";
-let title = document.querySelector('#content');
+let verse = ("content1.txt")  // sets default verse element
+$("#content-menu").val(verse); // changes menu option to default
+$("#choose-content").load(verse);   // retrieves only default element
 
+$("#content-menu").change(function() {
+verse = $(this).val();
+$("#choose-content").load(verse); 
+});
