6a789c5915f4b73de00fcc066fb9feb5262fc725
Cassy Smithies (RIT Student) <cls8313@ad.rit.edu>
2018-12-06 15:03:13 -0500

    adding jquery

---
+++ fall2018final-06/fall2018final/fall2018final.js
@@ -0,0 +1,73 @@
+
+$(".mainmenu").click(function () { //event handler
+    $(this).next(".submenu").slideToggle(); //toggles hide/show
+});
+
+
+
+
+
+let content = ("article0.txt")
+$("article").load(content);
+
+
+function radioClicked() {
+	content = this.value;
+    $("article").load(content);
+}
+
+
+let val =  0;
+$("#clickme").click(function() {
+    $('#count').html(function(i, val) { return +val+1 });
+})
+                   
+                   
+//let currentVal = 0;
+//function buttonClicked(){
+//	"#count".innerHTML = currentVal;
+//	}
+//button.addEventListener("click", buttonClicked);
+
+
+/*
+            <h2>Click a Button to Change the Article</h2>
+            <form>
+                <input type="radio" name="content" value="article1.txt">Article 1<br>
+                <input type="radio" name="content" value="article2.txt">Article 2
+            </form>
+            */
+
+/*
+<div id="colorform">
+	<legend>Choose your favorite color!</legend>
+	<p><input type="radio" name="colorGroup" value="red" /> Red</p>
+	<p><input type="radio" name="colorGroup" value="green" /> Green</p>
+	<p><input type="radio" name="colorGroup" value="blue" /> Blue</p>
+	<p><button id="colorButton" type="button">Submit</button></p>
+	<p id="info"></p>
+</div>
+
+
+let radioQ = document.querySelectorAll('#colorform > p > input');
+let textBox = document.querySelector('#colorform > #info');
+let legend = document.querySelector('#colorform > legend');
+
+function radioClicked() {
+    textBox.innerHTML = "You have selected the color " + this.value;
+	currentVal = this.value;
+}
+
+for(let i = 0; i < radioQ.length; i++){
+	radioQ[i].addEventListener("click", radioClicked);
+}
+
+let button = document.querySelector('#colorform > p> button');
+let currentVal;
+function buttonClicked(){
+	textBox.innerHTML = "Your FINAL CHOICE is " + currentVal;
+	legend.style.color = currentVal;
+	}
+
+button.addEventListener("click", buttonClicked);
+*/
