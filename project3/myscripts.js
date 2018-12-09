0e11c802139d8f68da0f31e728a28422ebd7d552
Cassycat99 <Cassycat99@yahoo.com>
2018-12-09 17:09:56 -0500

    adding comments

--- project3/myscripts.js
+++ project3/myscripts.js
@@ -1,18 +1,20 @@
-    
+    /*This function is used to create the navigation interaction on every page*/
     $('.menu-btn').on('click', function(e) {
-        e.preventDefault();
-        $(this).toggleClass('menu-btn_active');
-        $('.menu').toggleClass('menu_active');
+        e.preventDefault(); /*prevents error*/
+        $(this).toggleClass('menu-btn_active'); /*changes class*/
+        $('.menu').toggleClass('menu_active'); /*changes class*/
     });
 
 
 
 
-
+    /*If a project is selected, that project's content will no longer have a hide class on*/
+    /*Selecting a different project will hide the first one*/
+    /*hoverTitleBold changes the hoverTitle to bold to know which project is currently selected*/
     $(document).ready(function() {
-        $("#click1").click(function() {
-            $(".myClass1").toggleClass("hide");
-            $("#click1 .hoverTitle").toggleClass("hoverTitleBold");
+        $("#click1").click(function() { /*event handler*/
+            $(".myClass1").toggleClass("hide");  /*changes class*/
+            $("#click1 .hoverTitle").toggleClass("hoverTitleBold"); /*changes bold text*/
 
             if ($('#2').hasClass('hide')) {} else {
                 $("#2").toggleClass("hide");
@@ -27,6 +29,7 @@
         });
     });
 
+    /*repeat of above but project 2*/
     $(document).ready(function() {
         $("#click2").click(function() {
             $(".myClass2").toggleClass("hide");
@@ -45,6 +48,7 @@
         });
     });
 
+/*repeat of above but project 3*/
     $(document).ready(function() {
         $("#click3").click(function() {
             $(".myClass3").toggleClass("hide");
